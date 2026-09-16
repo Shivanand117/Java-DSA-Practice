59bf232d2e4c6e4c506be21d078244aa95166b72
Shivanand <shivanandavaradi243@gmail.com>
2026-09-16 23:57:47 +0530

    commiting

---
+++ src/DSA_Arrays/TrappingRainWater.java
@@ -0,0 +1,65 @@
+package src.DSA_Arrays;
+
+import java.util.Scanner;
+
+public class TrappingRainWater {
+
+    public static int trap(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+
+        int leftMax = 0;
+        int rightMax = 0;
+
+        int water = 0;
+
+        while (left < right) {
+
+            if (height[left] < height[right]) {
+
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    water += leftMax - height[left];
+                }
+
+                left++;
+
+            } else {
+
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    water += rightMax - height[right];
+                }
+
+                right--;
+            }
+        }
+
+        return water;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] height = new int[n];
+
+        System.out.println("Enter heights:");
+
+        for (int i = 0; i < n; i++) {
+            height[i] = sc.nextInt();
+        }
+
+        int result = trap(height);
+
+        System.out.println("Trapped water = " + result);
+
+        sc.close();
+    }
+}
